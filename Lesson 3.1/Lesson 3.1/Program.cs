87af65bdd7fd1204ee49eca4277d444b503d6d1e
Цiмафей Каротчанка <Paskydina@yandex.ru>
2022-08-15 11:25:25 +0300

    initial commit

---
+++ Lesson 3.1/Lesson 3.1/Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите два целых числа");
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    if (a > b)
+    {
+        Console.WriteLine("Первое число больше");
+    }
+    if (a == b)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    if (a < b)
+    {
+        Console.WriteLine("Второе число больше");
+    }
+}
+
+
---
+++ Lesson 3/Lesson 3/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 2;
+int b = 3;
+
+Console.WriteLine($"{a}-{b}");
+
+(a, b) = (b, a);
+
+Console.WriteLine($"{a}-{b}");
