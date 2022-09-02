886fa714d41fa012bfda6f27b4b10075997bd7a6
Цiмафей Каротчанка <Paskydina@yandex.ru>
2022-09-02 16:20:13 +0300

    1

---
+++ ConsoleApp2 (Файлы)/ConsoleApp2 (Файлы)/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+
+
+string path = "D:\\TestFile.txt";
+string text = "Тима учится работать с файлами";
+
+using (StreamWriter writer = new StreamWriter(path, true))
+{
+     writer.WriteLineAsync("Тима учится работать с файлами");
+}
