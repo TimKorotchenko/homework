fd25dbd8389124b28c41e7e30f8161b692b82687
Цiмафей Каротчанка <Paskydina@yandex.ru>
2022-12-15 14:07:51 +0300

    1

---
+++ Курсовая/Курсовая/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Курсовая.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
