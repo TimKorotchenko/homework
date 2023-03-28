70462f45bcdee93196f75654f7c018b6a4d791a4
Цiмафей Каротчанка <Paskydina@yandex.ru>
2023-03-28 15:16:51 +0300

    1

---
+++ BookStore/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        private void AddBindings()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book { Name = "Язык программирования С#", Author = "Троелсон", Prise = 1179},
+                new Book { Name = "Платформа .NET", Author = "Уотсон", Prise = 1155 },
+                new Book { Name = "Асинхронное программирование", Author = "Дэвис", Prise = 1166 }
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+    }
+
+}
